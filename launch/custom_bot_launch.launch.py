d39f87338f26f23f8089a91d31b01a863e62533c
dzinobile <dzinobile@gmail.com>
2026-01-28 13:46:44 -0500

    can visualize lidar if manually spin static transform publisher from base_link to lidar_frame

--- launch/custom_bot_launch.launch.py
+++ launch/custom_bot_launch.launch.py
@@ -1,61 +1,61 @@
-# import os
-# import launch
-# from launch import LaunchDescription
-# from ament_index_python.packages import get_package_share_directory
-# from webots_ros2_driver.webots_launcher import WebotsLauncher
-# from webots_ros2_driver.webots_controller import WebotsController
-# from launch_ros.actions import Node
+import os
+import launch
+from launch import LaunchDescription
+from ament_index_python.packages import get_package_share_directory
+from webots_ros2_driver.webots_launcher import WebotsLauncher
+from webots_ros2_driver.webots_controller import WebotsController
+from launch_ros.actions import Node
 
 
-# def generate_launch_description():
+def generate_launch_description():
     
-#     package_dir = get_package_share_directory('webots_diffdrive')
-#     robot_description_path = os.path.join(package_dir, 'resource', 'custom_robot.urdf')
-#     webots = WebotsLauncher(
-#         world=os.path.join(package_dir, 'worlds', 'custom_world.wbt')
-#     )
+    package_dir = get_package_share_directory('webots_diffdrive')
+    robot_description_path = os.path.join(package_dir, 'resource', 'custom_robot.urdf')
+    webots = WebotsLauncher(
+        world=os.path.join(package_dir, 'worlds', 'custom_world.wbt')
+    )
 
-#     robot_drivers = []
-#     tf_broadcasters = []
-#     world_to_robots = []
-#     for i in range (1,3):
-#         custom_robot_driver = WebotsController(
-#             robot_name=f'robot{i}',
-#             parameters=[
-#                 {'robot_description': robot_description_path},
-#             ],
-#             namespace=f'robot{i}'
+    robot_drivers = []
+    tf_broadcasters = []
+    world_to_robots = []
+    for i in range (1,3):
+        custom_robot_driver = WebotsController(
+            robot_name=f'robot{i}',
+            parameters=[
+                {'robot_description': robot_description_path},
+            ],
+            namespace=f'robot{i}'
 
-#         )
-#         robot_drivers.append(custom_robot_driver)
+        )
+        robot_drivers.append(custom_robot_driver)
 
-#         lidar_tf_broadcaster = Node(
-#             package='webots_diffdrive',
-#             executable='lidar_tf_broadcaster',
-#             output='screen',
-#             namespace=f'robot{i}',
-#             parameters=[{'robot_name': f'robot{i}'}],
-#         )
-#         tf_broadcasters.append(lidar_tf_broadcaster)
+        # lidar_tf_broadcaster = Node(
+        #     package='tf2_ros',
+        #     executable='static_transform_publisher',
+        #     output='screen',
+        #     namespace=f'robot{i}',
+        #     parameters=[{'robot_name': f'robot{i}'}],
+        # )
+        # tf_broadcasters.append(lidar_tf_broadcaster)
 
-#         world_to_robot = Node(
-#             package='tf2_ros',
-#             executable='static_transform_publisher',
-#             name=f'world_to_robot{i}_broadcaster',
-#             arguments=['0', '0', '0', '0', '0', '0', 'world', f'robot{i}/base_link']
-#         )
-#         world_to_robots.append(world_to_robot)
+        # world_to_robot = Node(
+        #     package='tf2_ros',
+        #     executable='static_transform_publisher',
+        #     name=f'world_to_robot{i}_broadcaster',
+        #     arguments=['0', '0', '0', '0', '0', '0', 'world', f'robot{i}/base_link']
+        # )
+        # world_to_robots.append(world_to_robot)
 
 
-#     return LaunchDescription([
-#         webots,
-#         *robot_drivers,
-#         *tf_broadcasters,
-#         *world_to_robots,
-#         launch.actions.RegisterEventHandler(
-#             event_handler=launch.event_handlers.OnProcessExit(
-#                 target_action=webots,
-#                 on_exit=[launch.actions.EmitEvent(event=launch.events.Shutdown())],
-#             )
-#         )
-#     ])
+    return LaunchDescription([
+        webots,
+        *robot_drivers,
+        # *tf_broadcasters,
+        # *world_to_robots,
+        launch.actions.RegisterEventHandler(
+            event_handler=launch.event_handlers.OnProcessExit(
+                target_action=webots,
+                on_exit=[launch.actions.EmitEvent(event=launch.events.Shutdown())],
+            )
+        )
+    ])
--- src/SensorTfBroadcaster.cpp
+++ src/SensorTfBroadcaster.cpp
@@ -1,62 +1,62 @@
-#include "rclcpp/rclcpp.hpp"
-#include "tf2_ros/static_transform_broadcaster.h"
-#include "geometry_msgs/msg/transform_stamped.hpp"
+// #include "rclcpp/rclcpp.hpp"
+// #include "tf2_ros/static_transform_broadcaster.h"
+// #include "geometry_msgs/msg/transform_stamped.hpp"
 
-class LidarTfBroadcaster : public rclcpp::Node {
-public:
-  LidarTfBroadcaster() : Node("sensor_tf_broadcaster") {
-    tf_static_broadcaster_ = std::make_shared<tf2_ros::StaticTransformBroadcaster>(this);
+// class LidarTfBroadcaster : public rclcpp::Node {
+// public:
+//   LidarTfBroadcaster() : Node("sensor_tf_broadcaster") {
+//     tf_static_broadcaster_ = std::make_shared<tf2_ros::StaticTransformBroadcaster>(this);
     
-    // Publish static transforms for sensors
-    publish_sensor_transforms();
-  }
+//     // Publish static transforms for sensors
+//     publish_sensor_transforms();
+//   }
 
-private:
-  void publish_sensor_transforms() {
-    std::vector<geometry_msgs::msg::TransformStamped> transforms;
+// private:
+//   void publish_sensor_transforms() {
+//     std::vector<geometry_msgs::msg::TransformStamped> transforms;
 
-    std::string ns = this->get_namespace();
-    if (ns == "/") ns = "";
-    else if (!ns.empty() && ns[0] == '/') ns = ns.substr(1);
+//     std::string ns = this->get_namespace();
+//     if (ns == "/") ns = "";
+//     else if (!ns.empty() && ns[0] == '/') ns = ns.substr(1);
 
-    std::string base_frame = ns.empty() ? "base_link" : ns + "/base_link";
+//     std::string base_frame = ns.empty() ? "base_link" : ns + "/base_link";
     
-    // Left sensor transform
-    geometry_msgs::msg::TransformStamped left_sensor_tf;
-    left_sensor_tf.header.stamp = this->get_clock()->now();
-    left_sensor_tf.header.frame_id = "base_link";
-    left_sensor_tf.child_frame_id = "left_sensor_frame";
-    // Adjust these values based on your robot's sensor positions
-    left_sensor_tf.transform.translation.x = 0.05;
-    left_sensor_tf.transform.translation.y = 0.02;
-    left_sensor_tf.transform.translation.z = 0.01;
-    left_sensor_tf.transform.rotation.w = 1.0;
-    transforms.push_back(left_sensor_tf);
+//     // Left sensor transform
+//     geometry_msgs::msg::TransformStamped left_sensor_tf;
+//     left_sensor_tf.header.stamp = this->get_clock()->now();
+//     left_sensor_tf.header.frame_id = "base_link";
+//     left_sensor_tf.child_frame_id = "left_sensor_frame";
+//     // Adjust these values based on your robot's sensor positions
+//     left_sensor_tf.transform.translation.x = 0.05;
+//     left_sensor_tf.transform.translation.y = 0.02;
+//     left_sensor_tf.transform.translation.z = 0.01;
+//     left_sensor_tf.transform.rotation.w = 1.0;
+//     transforms.push_back(left_sensor_tf);
     
-    // Right sensor transform
-    geometry_msgs::msg::TransformStamped right_sensor_tf;
-    right_sensor_tf.header.stamp = this->get_clock()->now();
-    right_sensor_tf.header.frame_id = "base_link";
-    right_sensor_tf.child_frame_id = "right_sensor_frame";
-    // Adjust these values based on your robot's sensor positions
-    right_sensor_tf.transform.translation.x = 0.05;
-    right_sensor_tf.transform.translation.y = -0.02;
-    right_sensor_tf.transform.translation.z = 0.01;
-    right_sensor_tf.transform.rotation.w = 1.0;
-    transforms.push_back(right_sensor_tf);
+//     // Right sensor transform
+//     geometry_msgs::msg::TransformStamped right_sensor_tf;
+//     right_sensor_tf.header.stamp = this->get_clock()->now();
+//     right_sensor_tf.header.frame_id = "base_link";
+//     right_sensor_tf.child_frame_id = "right_sensor_frame";
+//     // Adjust these values based on your robot's sensor positions
+//     right_sensor_tf.transform.translation.x = 0.05;
+//     right_sensor_tf.transform.translation.y = -0.02;
+//     right_sensor_tf.transform.translation.z = 0.01;
+//     right_sensor_tf.transform.rotation.w = 1.0;
+//     transforms.push_back(right_sensor_tf);
     
-    // Broadcast all transforms
-    tf_static_broadcaster_->sendTransform(transforms);
-    RCLCPP_INFO(this->get_logger(), "Published sensor transforms");
-  }
+//     // Broadcast all transforms
+//     tf_static_broadcaster_->sendTransform(transforms);
+//     RCLCPP_INFO(this->get_logger(), "Published sensor transforms");
+//   }
 
-  std::shared_ptr<tf2_ros::StaticTransformBroadcaster> tf_static_broadcaster_;
-};
+//   std::shared_ptr<tf2_ros::StaticTransformBroadcaster> tf_static_broadcaster_;
+// };
 
-int main(int argc, char **argv) {
-  rclcpp::init(argc, argv);
-  auto node = std::make_shared<LidarTfBroadcaster>();
-  rclcpp::spin(node);
-  rclcpp::shutdown();
-  return 0;
-}
+// int main(int argc, char **argv) {
+//   rclcpp::init(argc, argv);
+//   auto node = std::make_shared<LidarTfBroadcaster>();
+//   rclcpp::spin(node);
+//   rclcpp::shutdown();
+//   return 0;
+// }
